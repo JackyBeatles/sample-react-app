1ce1eeea572595a67783d0bd8641cc59e3c81047
JackyBeatles <JackyBeatles@TeamCity>
2022-03-22 09:32:24 +0000

    TeamCity change in 'Sample React App' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -35,6 +36,12 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            scriptContent = "npm install"
+        }
     }
 
     triggers {
